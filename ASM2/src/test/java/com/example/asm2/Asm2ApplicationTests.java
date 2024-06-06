fdfbda7fb05074056c7674489e1226f0ea5dd7b7
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-06 09:53:07 +0700

    .

---
+++ ASM2/src/test/java/com/example/asm2/Asm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asm2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
